4006feffb3c71af327eea113412bab85c09c11a9
Luke Doman <doman.luke@gmail.com>
2017-02-22 12:31:19 -0500

    Finished p1 and intial work on p2

--- cv_b457/a2/a2.py
+++ cv_b457/a2/a2.py
@@ -12,127 +12,125 @@ from pylab import *
 from scipy.ndimage import filters
 from scipy.signal import convolve2d
 
-# Problem 1.1
-def p1(im):
-	ar_im = array(im)
+def generate_filter(sigma, gradient, display = False):
+	"""
+	Generates and shows image kernel with specified parameters.
 
-	grad_1 = [-1,1]
-	filter_base = np.zeros(shape=(25,25))
-	imx = np.zeros(ar_im.shape)
-	sigma = 2
-	gaus_1 = filters.gaussian_filter(ar_im, (sigma,sigma), (0,1), imx)
-	#new_filter = convolve2d(grad_1, gaus_1, mode='same')
-	#gaus_1.dtype = np.uint8
-	imshow(gaus_1, cmap='gray')
-	show()	
+	Args:
+		sigma (int): Sigma to be used for applying Gaussian filter
+		gradient (2d array): Gradient to convolve Gaussian filter with
+		display (bool): Whether or not to display new filter
+
+	Returns:
+		Generated filter
+	"""
+	# Create base filter - all black with center white pixel
+	filter_base = np.zeros(shape=(51,51))
+	center_pixel = int(len(filter_base)/2)
+	filter_base[center_pixel][center_pixel] = 255
+
+	gaus = filters.gaussian_filter(filter_base, (sigma,sigma))
+	new_filter = convolve2d(gaus, gradient, mode='same')
+
+	if display:
+		imshow(new_filter, cmap='gray')
+		show()
+
+	return new_filter
+
+def filter_diff(f1, f2, display = False):
+	"""
+	Generates a filter based on the differences between two filters.
+
+	Args:
+		f1 (filter -> 2d array): Filter 1
+		f2 (filter -> 2d array): Filter 2
+		display (bool): Whether or not to display new filter
+
+	Returns:
+		New filter (2d array)
+	"""
+	size_x = len(f1)
+	size_y = len(f1[0])
+
+	new_filter = np.zeros(shape=(size_x,size_y))
+
+	for i in range(0, size_y):
+		for j in range(0, size_x):
+			new_filter[i][j] = 255 - abs(f1[i][j] - f2[i][j])
+
+	if display:
+		imshow(new_filter, cmap='gray')
+		show()
+
+	return new_filter
+
+def array_to_image(arr):
+	"""
+	Takes an 2D numpy array and converts it to an image. 
+
+	Args:
+		arr (numpy 2d array): Array to convert
+
+	Returns:
+		Image
+	"""
+	data = (arr - arr.min())
+	data *= 255/(data.max() - data.min()) 
+	img = Image.fromarray(data, 'L')
+	return img
+
+# Problem 1.1 - generate all the filters
+def p1():
+	kernels = []
+	sigmas = [2,4,8]
+	gradients = [[[-1,1]],[[-1,0],[1,0]]]
+
+	# Derivative of Gaussian filters
+	for sig in sigmas:
+		for grad in gradients:
+			kernels.append(generate_filter(sig, grad))
+
+	# Center surround filters
+	gaussians = []
+	for sig in sigmas:
+		gaussians.append(generate_filter(sig, [[1]]))
+
+	kernels.append(filter_diff(gaussians[0],gaussians[1]))
+	kernels.append(filter_diff(gaussians[1],gaussians[2]))
+
+	return kernels
 
 def p2(im):
 	ar_im = array(im)
+	kernels = p1()
 	results = []
 	results_img = []
-	for f in filters:
-		result = convolve2d(im, f)
+
+	for k in kernels:
+		result = convolve2d(ar_im, k, mode = 'same')
 		results.append(result)
-		results_img.append(arr2im(result))
+		results_img.append(array_to_image(result))
+
+	for res in results_img:
+		imshow(res)
+		show()
 
 def euclidean_distance(v1, v2):
-"""
-Calculates euclidean distance between (2) 8-D vectors
+	"""
+	Calculates euclidean distance between (2) 8-D vectors
 
-Args:
-	v1 (vector): Vector of 
-"""
+	Args:
+		v1 (vector): Vector of 
+	"""
+	# TODO
 
 def p3(im):
 	ar_im = array(im)
 	
-
-def convolve(im, fil):
-	"""
-	Takes two numpy arrays and applies convolution to them.
-	
-	Args:
-		im (numpy array): Image to apply filter to 
-		fil (numpy array): Filter to apply to image
-
-	Returns:
-		Numpy array that is the image with the apllied filter
-	"""
-	ar_im = array(im)
-	im_lenx = len(ar_im)
-	im_leny = len(ar_im[0])
-
-	ar_ret = numpy.zeros(shape=(im_lenx,im_leny))
-
-	# Iterate over each pixel of image
-	for i in range(0,im_lenx):
-		for j in range(0,im_leny):
-			new_pixel_value = 0
-			for x in range(0,len(fil)):
-				for y in range(0,len(fil[0])):
-					new_pixel_value = new_pixel_value + (fil[x][y]*ar_im[i-x][j-y])
-
-			ar_ret[i][j] = new_pixel_value
-
-	return ar_ret
 	
 
 if __name__ == '__main__':
 	im = Image.open('zebra.jpg').convert('L')
-	p1(im)
-
-
-
-#takes an 2D numoy array and converts it to an image
-def arr2im( data ):
-      data = (data - data.min())
-      data *= 255/(data.max() - data.min()) 
-      img = Image.fromarray(data, 'L') #the image will be saved in grayscale mode "L"
-      return img
-"""
-img = arr2im(data)
-img.save('my.png')
-img.show()
-
-
-
-# Problem 0
-def p0():
-	# 0.1
-	im_empire = Image.open('empire.jpg')
-
-	# 0.2
-	empire_cropped = im_empire.crop((100, 100, 400, 400))
-	empire_cropped.save("empire_cropped.jpg")
-
-	empire_cropped_rot = empire_cropped.rotate(45)
-	empire_cropped_rot.save("empire_cropped_rotated.jpg")
-
-	# 0.3
-	ar_emp = array(im_empire)
-	ar_emp_crop = array(empire_cropped)
-	ar_emp_crop_rot = array(empire_cropped_rot)
-
-	imshow(ar_emp)
-	show()
-	imshow(ar_emp_crop)
-	show()
-	imshow(ar_emp_crop_rot)
-	show()
-	
-	# 0.4
-	empire_gray = im_empire.convert('L')
-	empire_gray.save("empire_gray.jpg")
-
-	# 0.5
-	ar_emp_gray = array(empire_gray)
-	ar_emp_new = toGray(im_empire)
-
-	imshow(ar_emp_gray, cmap='gray')
-	show()	
-	imshow(ar_emp_new, cmap='gray')
-	show()
-
-"""
-
+	p2(im)
 
