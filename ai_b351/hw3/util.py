dc415defb02150c46b2d94923a78b7d1a258a197
Luke Doman <doman.luke@gmail.com>
2017-02-27 14:08:55 -0500

    Removing unused code

--- ai_b351/hw3/util.py
+++ ai_b351/hw3/util.py
@@ -1,5 +1,3 @@
-from Queue import PriorityQueue
-
 class Node(object):
 
     def __init__(self, name, neighbors = [], cost = 1):
@@ -83,76 +81,6 @@ class Graph(object):
 
     def reach(self, node1, node2):
         return (node1 in self.DFS(node2))
-
-    def min_distance(self, current_node, end_node, visited = [], next_node = None):
-        if current_node == end_node:
-            return 0
-        elif end_node.get_name() in current_node.get_neighbors():
-            return self.edges[(current_node.get_name(), end_node.get_name())]
-        else:
-            if next_node is None:
-                neighbors = [self.get_node(n) for n in current_node.get_neighbors()]
-                ret = map(lambda n: self.min_distance(current_node, end_node, visited, n), neighbors)
-                return min(ret), visited
-            elif next_node in visited:
-                return 100 # Penalize cyclic paths
-            else:
-                visited.append(current_node)
-                return self.edges[(current_node.get_name(), next_node.get_name())] + \
-                    self.min_distance(next_node, end_node, visited)
-
-    # TODO: Not optimal. Need to account for blocks in maze.
-    def h_hat(self, node1, node2):
-		"""
-		Calculates the total number of moves required for node1 to reach node2.
-
-		Args:
-			node1 (Node): Start node to measure moves from
-			node2 (Node): End node to measure moves to
-
-		Returns:
-			Int: number of moves
-		"""
-		moves_x = node1.get_name()[0] - node2.get_name()[0]
-		moves_y = node1.get_name()[1] - node2.get_name()[1]
-		ret = abs(moves_x) + abs(moves_y)
-		return ret 
-
-    def a_star(self, start_node, end_node):
-		"""
-		Finds the optimal path between 2 nodes
-
-		Args:
-			start_node (Node): Node to start search at
-			end_node (Node): End node for search 
-
-		Returns:
-			Bool: Whether end node is reachable from start node
-			List: Optimal path
-		"""
-		reachable = False
-		visited = []
-		frontier = PriorityQueue()
-		frontier.put(start_node, 0)
-		cost = {}
-		cost[start_node] = 0
-
-		while not frontier.empty():
-			current_node = frontier.get()
-			visited.append(current_node)
-			if current_node == end_node:
-				reachable = True
-				break
-
-			for neigh in [self.get_node(n) for n in current_node.get_neighbors()]:
-				g_hat = cost[current_node] + self.edges[(current_node.get_name(), neigh.get_name())]
-				if neigh not in cost or g_hat < cost[neigh]:
-					cost[neigh] = g_hat
-					f_hat = g_hat + self.h_hat(end_node, neigh)
-					frontier.put(neigh, f_hat)
-
-		return reachable, visited
-
 
 class Stack:
 
