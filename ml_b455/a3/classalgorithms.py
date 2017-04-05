44875fb6289b79ad3ac0e5860a59ced5873513af
Luke Doman <doman.luke@gmail.com>
2017-04-05 09:38:31 -0400

    Updates

--- ml_b455/a3/classalgorithms.py
+++ ml_b455/a3/classalgorithms.py
@@ -180,13 +180,13 @@ class LogitReg(Classifier):
 
         for j in range(20):
             for i in range(Xtrain.shape[0]):
-                xvec = np.dot(Xtrain[i], self.weights)  #(1*9) * (9*1) = 500*1
+                xvec = np.dot(Xtrain[i], self.weights)
                 delta = np.divide((2*ytrain[i]-1)*np.sqrt(np.square(xvec)+1)-xvec,np.square(xvec)+1)
                 delta = np.dot(Xtrain[i].T,delta)
                 first_term = np.divide((2*ytrain[i]-1)*xvec - np.sqrt(np.square(xvec)+1)-xvec,np.power(np.square(xvec)+1,3/2))
                 second_term = 2*xvec*np.divide((2*ytrain[i]-1)*np.sqrt(np.square(xvec)+1)-xvec,np.square(np.square(xvec)+1))
                 hessian = np.dot(Xtrain[i].T,Xtrain[i])*(first_term-second_term)
-                self.weights = self.weights + self.step_size * delta/hessian #(500*9) * (500*1) = (9*1)
+                self.weights = self.weights + self.step_size * delta/hessian
 
     def predict(self, Xtest):
         # print self.weights
